5dfa5b6aefd813fc82dbd2097ba8b8b6220e6bf3
Marjan Celi <marjanazliga@gmail.com>
2019-04-12 09:19:12 -0500

    Segunda Clase

---
+++ Ejemplo021/src/ejemplo021/Ejemplo021a.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo021;
+
+/**
+ *
+ * @author Personal
+ */
+public class Ejemplo021a {
+    public static void main(String[] args) {
+       // este metodo es el principal de mi clase
+       String nombrePersona; // es la variable para almacenar un nombre 
+       String apellidoPersona; 
+       
+       nombrePersona = "Marjan Aldair ";
+       apellidoPersona = "Celi Salinas";
+       
+        System.out.println("Mi Nombre es :"+nombrePersona+"\n\n\t Mi apellido es :"+
+                apellidoPersona);
+    }
+    
+}
